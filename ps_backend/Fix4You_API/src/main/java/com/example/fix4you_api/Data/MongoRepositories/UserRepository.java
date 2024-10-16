d3302f6842559d7f57982107453a6b5494c70b98
André Luís <andre.luis@onesource.pt>
2024-10-16 13:12:04 +0100

    professionals crud

--- ps_backend/Fix4You_API/src/main/java/com/example/fix4you_api/Data/MongoRepositories/UserRepository.java
+++ ps_backend/Fix4You_API/src/main/java/com/example/fix4you_api/Data/MongoRepositories/UserRepository.java
@@ -1,7 +1,11 @@
 package com.example.fix4you_api.Data.MongoRepositories;
 
+import com.example.fix4you_api.Data.Enums.EnumUserType;
 import com.example.fix4you_api.Data.Models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface UserRepository extends MongoRepository<User, String> {
+    List<User> findByUserType(EnumUserType userType);
 }
