2d1294c93c58a319cab4d9de5067947567f15e9c
Eduardo Correia <eduardo2002correia.com>
2024-10-31 19:27:44 +0000

    Update Professional update on controller

---
+++ ps_backend/Fix4You_API/src/main/java/com/example/fix4you_api/Service/Professional/ProfessionalService.java
@@ -0,0 +1,20 @@
+package com.example.fix4you_api.Service.Professional;
+
+import com.example.fix4you_api.Data.Models.Professional;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ProfessionalService {
+    List<Professional> getAllProfessionals();
+
+    Professional getProfessionalById(String id);
+
+    Professional createProfessional(Professional professional);
+
+    Professional updateProfessional(String id, Professional professional);
+
+    Professional partialUpdateProfessional(String id, Map<String, Object> updates);
+
+    void deleteProfessional(String id);
+}
